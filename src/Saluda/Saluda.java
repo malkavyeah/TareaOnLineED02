1c82362579af3ea4726cea8204f27d098b411b8c
Alberto Valdivia <albertovaldiviasaez@gmail.com>
2016-11-27 03:16:26 +0100

    Saludo personal

--- src/Saluda/Saluda.java
+++ src/Saluda/Saluda.java
@@ -4,7 +4,8 @@ public class Saluda {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("¡Hola!");
+		System.out.println("¿Cual es tu nombre?");
+		System.out.println("¡Hola!");
 	}
 
 }
