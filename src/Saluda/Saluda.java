d1bb5e2f32d3bc3b4873ea16e2b78627ff7be677
Alberto Valdivia <albertovaldiviasaez@gmail.com>
2016-11-27 04:23:32 +0100

    Creamos método pideNombre()

--- src/Saluda/Saluda.java
+++ src/Saluda/Saluda.java
@@ -3,15 +3,37 @@ package Saluda;
 import java.util.Scanner;
 
 public class Saluda {
-
+	String nombre="";
+	
+	public  String getNombre() {
+		 return nombre;
+		 
+		 
+	 }
+	 
+	 public void setNombre(String nombre){
+		 nombre=nombre;
+	 }
+	 
+	 public void pideNombre(){
+		 System.out.println("¿Cual es tu nombre?");
+		 Scanner entrada=new Scanner(System.in);
+		 nombre=entrada.next();
+	 }
+	 
+	 public String toString(){
+			String mensaje="¡Hola!"+nombre; 
+			return mensaje;
+		 }
+		 
+	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String nombre;
-		   
-		  Scanner entrada=new Scanner(System.in);
-		System.out.println("¿Cual es tu nombre?");
-		nombre= entrada.next();
-		System.out.println("¡Hola"+nombre+"!");
+		
+		
+		
+		
+		 
 	}
 
 }
