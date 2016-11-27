3cdd1c5e03c8c748de6ab19ac42f0fcf4b36993c
Alberto Valdivia <albertovaldiviasaez@gmail.com>
2016-11-27 04:06:49 +0100

    Encapsulamos la variable de clase _nombre

--- src/Saluda/Saluda.java
+++ src/Saluda/Saluda.java
@@ -1,11 +1,17 @@
 package Saluda;
 
+import java.util.Scanner;
+
 public class Saluda {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String nombre;
+		   
+		  Scanner entrada=new Scanner(System.in);
 		System.out.println("¿Cual es tu nombre?");
-		System.out.println("¡Hola!");
+		nombre= entrada.next();
+		System.out.println("¡Hola"+nombre+"!");
 	}
 
 }
